700fb9770282e9f4e848399f9727efd2e04c802a
cvelasquez <carlosrobertovelasquez@gmail.com>
2020-05-16 01:21:03 -0600

    Primer commit se agrega pagina de  inicio con personalizacion de boton de  carrito de compras

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "../src/App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/font-awesome/css/font-awesome.min.css";
+import Navbar from './component/Navbar';
+import ProductList from './component/ProductList';
+import Details from './component/Details';
+import Cart from './component/Cart';
+import Default from './component/Default';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar/>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={ProductList} />
+            <Route exact path="/Details" component={Details} />
+            <Route exact path="/Cart/" component={Cart} />
+            <Route  component={Default} />
+          </Switch>
+        </BrowserRouter>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/component/ProductList.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Product from "./Product";
+
+class ProductList extends Component {
+    render() {
+        return (
+           <Product />
+        );
+    }
+}
+
+export default ProductList;
